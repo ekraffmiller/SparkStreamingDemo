44f55b968fe5f67f5acd6bcf182a461f31d7ff22
ellenk <ekraffmiller@hmdc.harvard.edu>
2017-10-02 20:31:16 -0400

    update commit

--- src/main/java/edu/harvard/iq/sparkstreamingml/TweetRecord.java
+++ src/main/java/edu/harvard/iq/sparkstreamingml/TweetRecord.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Java Bean for Tweet data
  */
 package edu.harvard.iq.sparkstreamingml;
 
